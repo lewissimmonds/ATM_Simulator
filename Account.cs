04d4744e6d7f6ea8eac811e2add34937f36363ba
FinnbarHome <2481383@dundee.ac.uk>
2023-03-25 17:32:54 +0000

    added account class which stores pin and accountNum

---
+++ Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Simulator
+{
+    internal class Account
+    {
+        public int accountNum { get; set; }
+        public int pin { get; set; }
+
+        public Account(int accNum, int pinNum)
+        {
+            accountNum = accNum;
+            pin = pinNum;
+        }
+    }
+}
