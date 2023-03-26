d75e0c0c120f08b570cc2702dc592f399e880026
FinnbarHome <2481383@dundee.ac.uk>
2023-03-26 17:54:54 +0100

    Started on threads

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace ATM_Simulator
 {
     internal static class Program
@@ -11,7 +13,18 @@ namespace ATM_Simulator
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new ATM());
+
+            // Create two threads, each running its own instance of the application
+            Thread Atm0 = new Thread(() => Application.Run(new ATM()));
+            Thread Atm1 = new Thread(() => Application.Run(new ATM()));
+            Thread CenComp = new Thread(() => Application.Run(new ATM())); // REPLACE WITH CENTRAL COMPUTER APP!!!!!!!
+
+            // Start the threads
+            Atm0.Start();
+            Atm1.Start();
+            CenComp.Start();
+
+            
         }
     }
 }
